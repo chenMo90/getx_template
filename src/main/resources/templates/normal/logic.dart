7de3d1905c68c257821c8901f8ec51646b3d4e0f
jim <904165900@qq.com>
2025-07-01 11:17:46 +0800

    1

--- src/main/resources/templates/normal/logic.dart
+++ src/main/resources/templates/normal/logic.dart
@@ -4,4 +4,16 @@ import 'state.dart';
 
 class @nameLogic extends GetxController {
   final @nameState state = @nameState();
+
+  // 初始化
+  @override
+  void onInit() {
+    super.onInit();
+  }
+
+  // 释放
+  @override
+  void onClose() {
+    super.onClose();
+  }
 }
